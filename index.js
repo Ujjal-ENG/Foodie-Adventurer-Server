ee0a16126bb59eac605f45aa07a122d817a5d94a
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-05-01 20:41:55 +0600

    initial server is created and also default route set and the server port is 8080

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-// dependencies
-const http = require('http');
-// app object - module scaffolding
-const app = {};
-// configuration
-app.config = {};
+const express = require('express');
+const cors = require('cors');
+
+// initialize app
+const app = express();
+
+// initialize middleware
+app.use(cors());
+app.use(express.json());
+
+// default route
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello from Server!!!' });
+});
+
+// listen app
+app.listen(8080, () => {
+    console.log('Server is running on Port 8080');
+});
